2b5539128dcfe0e18de3efde4f9855a648779dd5
David Begin <davidmichaelbe@gmail.com>
2015-05-05 23:59:57 -0700

    Namespacing for fun and profit

--- src/main.rs
+++ src/main.rs
@@ -42,32 +42,40 @@ fn seperator() {
 }
 
 
-#[test]
-fn name_returns_the_users_full_name() {
-    let subject = users::User {
-        first_name: "David".to_string(),
-        last_name: "Begin".to_string(),
-        email: "david@example.com".to_string()
-    };
-    assert_eq!(subject.name(), "David Begin".to_string());
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use users;
+    use users::FullName;
+    use users::Email;
 
-#[test]
-fn email_returns_the_users_email() {
-    let subject = users::User {
-        first_name: "David".to_string(),
-        last_name: "Begin".to_string(),
-        email: "david@example.com".to_string()
-    };
-    assert_eq!(subject.email(), "david@example.com".to_string());
-}
+    #[test]
+    fn name_returns_the_users_full_name() {
+        let subject = users::User {
+            first_name: "David".to_string(),
+            last_name: "Begin".to_string(),
+            email: "david@example.com".to_string()
+        };
+        assert_eq!(subject.name(), "David Begin".to_string());
+    }
 
-// #[test]
-fn obfuscated_email_returns_a_gibberish_email() {
-    let subject = users::User {
-        first_name: "David".to_string(),
-        last_name: "Begin".to_string(),
-        email: "david@example.com".to_string()
-    };
-    assert_eq!(subject.email(), "xxx@example.com".to_string());
+    #[test]
+    fn email_returns_the_users_email() {
+        let subject = users::User {
+            first_name: "David".to_string(),
+            last_name: "Begin".to_string(),
+            email: "david@example.com".to_string()
+        };
+        assert_eq!(subject.email(), "david@example.com".to_string());
+    }
+
+    // #[test]
+    fn obfuscated_email_returns_a_gibberish_email() {
+        let subject = users::User {
+            first_name: "David".to_string(),
+            last_name: "Begin".to_string(),
+            email: "david@example.com".to_string()
+        };
+        assert_eq!(subject.email(), "xxx@example.com".to_string());
+    }
 }
