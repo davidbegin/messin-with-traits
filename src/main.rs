ed5c7c200450499b549137cb19ceeed1ea2503f3
David Begin <davidmichaelbe@gmail.com>
2015-06-10 17:13:32 -0700

    remembering how traits work again

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code, unused_variables)]
 #![feature(convert)]
 
 extern crate regex;
@@ -8,16 +9,47 @@ mod da_printers;
 fn main() {
     da_printers::print_title();
 
-    let david = users::User {
-        first_name: "David".to_string(),
-        last_name: "Begin".to_string(),
-        email: "david@example.com".to_string()
-    };
+    // let david = users::User {
+    //     first_name: "David".to_string(),
+    //     last_name: "Begin".to_string(),
+    //     email: "david@example.com".to_string()
+    // };
+    //
+    // let guest = users::Guest;
+    //
+    // da_printers::print_stats(david);
+    // da_printers::seperator();
+    // da_printers::print_stats(guest);
 
-    let guest = users::Guest;
+    trait_madness();
+    // trait_object_madness();
+}
 
-    da_printers::print_stats(david);
-    da_printers::seperator();
-    da_printers::print_stats(guest);
+fn trait_madness() {
+    let c1 = Circle { x: 5.1f64, y: 6.4f64, radius: 8.2f64 };
+    area_printer(c1);
+}
+
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
+
+fn area_printer<T: HasArea>(obj: T) {
+    println!("Area of c1: {}", obj.area());
+}
+
+fn trait_object_madness() {
 }
 
