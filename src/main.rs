7411dbf8bc27422d41526536d609c111d51fa54d
David Begin <davidmichaelbe@gmail.com>
2015-06-10 17:26:44 -0700

    building that muscle memory up, and my knowledge of lifetimes
      and borrowing has definitely gotten better, 
      but still needs a lot of word

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,16 @@ fn main() {
     // da_printers::print_stats(guest);
 
     trait_madness();
-    // trait_object_madness();
 }
 
 fn trait_madness() {
     let c1 = Circle { x: 5.1f64, y: 6.4f64, radius: 8.2f64 };
-    area_printer(c1);
+    area_printer(&c1);
+
+    let s1 = Square { side: 10.1, title: "Square".to_owned() };
+    area_printer(&s1);
+    title_printer(s1);
+    title_printer(c1);
 }
 
 struct Circle {
@@ -46,10 +50,37 @@ impl HasArea for Circle {
     }
 }
 
-fn area_printer<T: HasArea>(obj: T) {
-    println!("Area of c1: {}", obj.area());
+fn area_printer<T: HasArea>(obj: &T) {
+    println!("Area of obj: {}", obj.area());
 }
 
-fn trait_object_madness() {
+struct Square {
+    side: f64,
+    title: String,
 }
 
+impl HasArea for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+trait HasTitle {
+    fn title(self) -> String;
+}
+
+fn title_printer<T: HasTitle>(obj: T) {
+    println!("Title: {}", obj.title());
+}
+
+impl HasTitle for Square {
+    fn title(self) -> String {
+        self.title
+    }
+}
+
+impl HasTitle for Circle {
+    fn title(self) -> String {
+        "Circle".to_string()
+    }
+}
