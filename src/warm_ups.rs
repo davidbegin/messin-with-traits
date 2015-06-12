e5143a71080894514dd2b7bc338adf10e1d2ec7d
David Begin <davidmichaelbe@gmail.com>
2015-06-11 21:11:14 -0700

    I am suprised I pulled that off that quickly

--- src/warm_ups.rs
+++ src/warm_ups.rs
@@ -6,15 +6,33 @@ pub fn rep_one() {
     c1("fancy beat".to_string());
     c1(232.44f64);
     c1(b'a');
+
+    // I want to do vec, but it might be too complicatd for now!
+    //
+    // I need to get back to mess with vecs more.
+    //
+    // I am also out of practice
+    // let vec: Vec<i32> = vec![1];
+    c2(2, 1);
 }
 
 trait HasDoIt {
     fn do_it(&self);
 }
 
+trait HasYeahYeah {
+    fn yeah_yeah(&self) -> String;
+}
+
 impl HasDoIt for i32 {
     fn do_it(&self) {
         println!("I'm i32 and I'm doing it!");
+    }
+}
+
+impl HasYeahYeah for i32 {
+    fn yeah_yeah(&self) -> String {
+        "Yeah Yeah i32!".to_owned()
     }
 }
 
@@ -42,11 +60,11 @@ impl HasDoIt for u8 {
     }
 }
 
-// create a method that takes a generic
+// √ create a method that takes a generic
 
-// create a method that takes a genric bound by a trait
+// √ create a method that takes a genric bound by a trait
 
-// create 2 structs or types that fill that role
+// √ create 2 structs or types that fill that role
 // ...so structs can have traits, and types, structs are types...and...I am obviously
 //    missing osme crucial concepts.
 
@@ -55,7 +73,22 @@ fn c1<T: HasDoIt>(thang: T) {
   // what should I do with t
 }
 
-// what is something that I can do for all types
-// well lets implement something for all these types
+// create method that takes an utilizes two generics, 1 with a 1 trait, and the other with 2
 
-//...maybe start with 1
+// essentially fn foo<T: Thang, R: Thang + OtherThang>();
+
+// create a trait has a method that returns a value
+// ....wait or are traits only queries on internal state of the obj
+// the trait is implemented for?
+
+
+// wow its nice not having to think about a real life example.
+//
+// and I like this whole has thing, makes traits easier to comprehed
+// ...but is it an actula convention? I just generally want to see more
+// complex ways traits are used in real world examples.
+fn c2<T: HasDoIt, Z: HasDoIt + HasYeahYeah>(thang1: T, thang2: Z) {
+    thang1.do_it();
+    thang2.do_it();
+    println!("yeah yeah: {}", thang2.yeah_yeah());
+}
